293eaaa821634c3b6cf25054aebe4f1c4e32e570
Oliver Rapp <oliver@des-or-mad.net>
2016-01-08 23:42:08 +0100

    Added tmux.sh.

---
+++ tmux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SESSION=main
+tmux="tmux -2 -u -f $HOME/.tmux.conf"
+
+# if the session is already running, just attach to it.
+$tmux has-session -t $SESSION
+if [ $? -eq 0 ]; then
+       echo "Session $SESSION already exists. Attaching."
+       sleep 1
+       $tmux attach -t $SESSION
+       exit 0;
+fi
+
+# create a new session, named $SESSION, and detach from it
+$tmux new-session -d -s $SESSION
+$tmux new-window    -t $SESSION:0
+$tmux split-window  -v -t $SESSION:0
+$tmux attach -t $SESSION
+
