3e3cbdb15d5aac6f0c77c1dbd4adc504faf343f3
Oliver Rapp <oliver@des-or-mad.net>
2016-01-27 13:20:49 +0100

    added missing option to load ~/.tmux.conf in tmudev.sh

--- tmuxdev.sh
+++ tmuxdev.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 SESSION=`basename $PWD`
+tmux="tmux -2 -u -f $HOME/.tmux.conf"
 
 # vim
-tmux -2 new-session -d -s $SESSION -n 'vim'
-tmux split-window -t 1 -h -p 25
+$tmux -2 new-session -d -s $SESSION -n 'vim'
+$tmux split-window -t 1 -h -p 25
 
 #logs
-tmux new-window -t $SESSION -n 'logs'
+$tmux new-window -t $SESSION -n 'logs'
 
 # mysql
-tmux new-window -t $SESSION -n 'mysql'
-tmux split-window -h
+$tmux new-window -t $SESSION -n 'mysql'
+$tmux split-window -h
 
 # svn
-tmux new-window -t $SESSION -n 'svn'
-tmux split-window -h
+$tmux new-window -t $SESSION -n 'svn'
+$tmux split-window -h
 
 # attach to session
-tmux select-window -t $SESSION:1 \; select-pane -t 1
-tmux -2 attach-session -t $SESSION
+$tmux select-window -t $SESSION:1 \; select-pane -t 1
+$tmux attach-session -t $SESSION
